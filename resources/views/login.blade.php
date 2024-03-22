ed9e6af57b93cb8c7ec6d4731c512f71953f62ee
José Lucas dos Santos Cantão <cantao162@gmail.com>
2024-03-22 09:15:22 -0300

    error message

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,6 +2,24 @@
 @section('title', 'login')
 @section('content')
     <div class="container">
+      <div class="mt-5">
+        @if ($errors->any())
+        <div class="col-12">
+          @foreach ($errors->all() as $error)
+              <div class="alert alert-danger">{{$error}}</div>
+          @endforeach
+        </div>
+        @endif
+
+        @if (session()->has('error'))
+            <div class="alert alert-danger">{{session('error')}}</div>
+        @endif
+
+        @if (session()->has('success'))
+            <div class="alert alert-success">{{session('success')}}</div>
+        @endif
+
+      </div>
         <form class="mx-auto mt-3" style="width:500px" action="{{route('login.post')}}" method="POST">
             @csrf
             <div class="mb-3">
